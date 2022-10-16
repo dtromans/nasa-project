7c12b686460e7611ed0f39030eb049d85e98951a
dtromans <29309408+dtromans@users.noreply.github.com>
2022-10-15 20:43:27 -0400

    completed mongoose/mongoDB integration into API; updated tests accordingly

---
+++ server/src/services/mongo.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const MONGO_URL = 'mongodb+srv://nasa-api:w5e6UsdNdePVstrM@nasacluster.uw0g7vc.mongodb.net/nasa?retryWrites=true&w=majority';
+
+mongoose.connection.on('open', () => {
+	console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+	console.error(err);
+});
+
+async function mongoConnect() {
+	await mongoose.connect(MONGO_URL);
+}
+
+async function mongoDisconnect() {
+	await mongoose.disconnect();
+}
+
+module.exports = {
+	mongoConnect,
+	mongoDisconnect,
+};
